43d26bc52a48308372954748a428ce196bc83601
Taiwo Muraina <133337460+tmuraina@users.noreply.github.com>
2025-09-21 23:22:38 +0100

    Update urls.py

--- messaging_app/urls.py
+++ messaging_app/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/", include("chats.urls")),
+    path('admin/', admin.site.urls),
+    path('api/', include('chats.urls')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
