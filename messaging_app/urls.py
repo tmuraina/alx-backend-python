08658787cee302ff94b2c267a28161d0386836d4
Taiwo Muraina <133337460+tmuraina@users.noreply.github.com>
2025-09-21 23:16:08 +0100

    Update urls.py

--- messaging_app/urls.py
+++ messaging_app/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('chats.urls')),
+    path("admin/", admin.site.urls),
+    path("api/", include("chats.urls")),
 ]
