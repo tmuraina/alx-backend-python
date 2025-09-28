b63b41794885d1e038741ec4c4f5422db92fcf6d
Taiwo Muraina <133337460+tmuraina@users.noreply.github.com>
2025-09-28 23:14:08 +0100

    Update settings.py

--- messaging_app/settings.py
+++ messaging_app/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
     
     # Third-party apps
     'rest_framework',
@@ -133,6 +134,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20
 }
@@ -149,5 +153,6 @@ CORS_ALLOW_CREDENTIALS = True
 
 # Custom User Model
 AUTH_USER_MODEL = 'chats.User'
+
 
 
