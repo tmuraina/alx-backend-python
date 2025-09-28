f9befd7761c03e40009196b7a4308988e8a7f70f
Taiwo Muraina <133337460+tmuraina@users.noreply.github.com>
2025-09-28 21:16:08 +0100

    Update apps.py

--- messaging_app/messaging_app/chats/apps.py
+++ messaging_app/messaging_app/chats/apps.py
@@ -1,1 +1,8 @@
- 
+# messaging_app/chats/apps.py
+
+from django.apps import AppConfig
+
+
+class ChatsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'chats' 
