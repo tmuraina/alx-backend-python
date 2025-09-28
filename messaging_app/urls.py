f56eb9aecd831050aaa8306f307efc3d36e33cea
Taiwo Muraina <133337460+tmuraina@users.noreply.github.com>
2025-09-28 23:06:56 +0100

    Update urls.py

--- messaging_app/urls.py
+++ messaging_app/urls.py
@@ -2,6 +2,12 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from chats.views import ConversationViewSet, MessageViewSet
+
+router = DefaultRouter()
+router.register(r'conversations', ConversationViewSet, basename='conversation')
+router.register(r'messages', MessageViewSet, basename='message')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
